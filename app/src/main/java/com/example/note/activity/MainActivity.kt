1a393a862a4a727dbb19b29ff16ef20fd1baa636
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-07-12 15:43:42 +0330

    update MainActivity.kt

--- app/src/main/java/com/example/note/activity/MainActivity.kt
+++ app/src/main/java/com/example/note/activity/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.note.activity
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.note.R
 import com.example.note.adapter.MyAdapter
 import com.example.note.database.NotesDao
 import com.example.note.database.NotesDatabase
 import com.example.note.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -37,20 +31,7 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
-        snackBar()
     }
 
     private fun selectFromDb() = notesDao.getAllNotes()
-    private fun snackBar() {
-        val snackBar =
-            Snackbar.make(binding.root, getString(R.string.enter_message), Snackbar.LENGTH_SHORT)
-                .setBackgroundTint(
-                    Color.GRAY
-                )
-        val params = snackBar.view.layoutParams as (FrameLayout.LayoutParams)
-        params.width = FrameLayout.LayoutParams.WRAP_CONTENT
-        params.gravity = Gravity.BOTTOM or Gravity.CENTER // Bottom centre
-        snackBar.view.background = ContextCompat.getDrawable(this, R.drawable.shape_round)
-        snackBar.show()
-    }
 }
