c95a477864ea2bdfcc99c67d1211664aa26cd192
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-07-14 16:47:13 +0330

    Update ApiService.kt

--- app/src/main/java/com/example/note/ApiService.kt
+++ app/src/main/java/com/example/note/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.note
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("Api/Market/Gold_Currency.php?key=Freeb4kFJyIjKGneMaRtkAE457lV9iON")
+    fun getData(): Call<DataClass>
 }
