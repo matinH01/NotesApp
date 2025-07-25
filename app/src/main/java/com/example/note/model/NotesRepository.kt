13bcacc693dde532cf5be3d3aeed784b5f402196
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-07-25 16:16:43 +0330

    Update NotesRepository.kt

--- app/src/main/java/com/example/note/model/NotesRepository.kt
+++ app/src/main/java/com/example/note/model/NotesRepository.kt
@@ -1,5 +1,14 @@
 package com.example.note.model
 
-class NotesRepository() {
+class NotesRepository(private val dao: NotesDao) {
 
+    val notes = dao.getAllNotes()
+
+    fun insert(notes: NotesData) {
+        dao.insertNotes(notes)
+    }
+
+    fun deleteNotes(note: NotesData) {
+        dao.deleteNotes(note)
+    }
 }
