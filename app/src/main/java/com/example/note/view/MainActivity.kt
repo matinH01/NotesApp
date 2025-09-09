0d2a5ecb41e9a9d172f89e7dd3006c7ae60cde42
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-09 18:05:39 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/note/view/MainActivity.kt
+++ app/src/main/java/com/example/note/view/MainActivity.kt
@@ -2,24 +2,16 @@ package com.example.note.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.note.ApiService
 import com.example.note.R
 import com.example.note.adapter.MyAdapter
 import com.example.note.databinding.ActivityMainBinding
 import com.example.note.model.NotesDao
 import com.example.note.model.NotesData
 import com.example.note.model.NotesDatabase
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         notesDao = NotesDatabase.buildDatabase(this).getNotesDao()
@@ -34,14 +27,11 @@ class MainActivity : AppCompatActivity() {
 
         setRecyclerViewAdapter(noteData)
 
-
         binding.floatingActionButton.setOnClickListener {
             val intent = Intent(this, AddNotesActivity::class.java)
+            intent.putExtra("AddOrShow", true)
             startActivity(intent)
         }
-
-        getTimeFromServer()
-
     }
 
     private fun setRecyclerViewAdapter(noteData: MutableList<NotesData>) {
@@ -51,30 +41,5 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun selectFromDb() = notesDao.getAllNotes()
-
-    private fun getTimeFromServer() {
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .baseUrl("https://brsapi.ir/")
-            .build()
-
-        val apiService = retrofit.create(ApiService::class.java)
-        val call = apiService.getData()
-
-        call.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                if (response.body() != null && response.isSuccessful) {
-                    val jsonObject = JSONObject(response.body()!!)
-                    binding.textDate =
-                        "${jsonObject.getJSONArray("gold").getJSONObject(0).get("date")}"
-                    binding.visibility = View.INVISIBLE
-                }
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.e("API_ERROR", t.message ?: "Unknown error")
-            }
-        })
-    }
 
 }
