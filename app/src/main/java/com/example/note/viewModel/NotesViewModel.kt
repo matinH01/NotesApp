ee605900581a82b98c346b4d6995e09c78be810e
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-10 09:56:16 +0330

    Update NotesViewModel.kt

--- app/src/main/java/com/example/note/viewModel/NotesViewModel.kt
+++ app/src/main/java/com/example/note/viewModel/NotesViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.note.viewModel
 
-class NotesViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.note.state.NoteUiState
+
+class NotesViewModel : ViewModel() {
+    private val _uiState = MutableLiveData<NoteUiState>()
+    val uiState: LiveData<NoteUiState> = _uiState
+
+    fun setData() {
+        if (_uiState.value?.showOrAdd == true) {
+            _uiState.value = NoteUiState(topPageText = "یادداشت جدید")
+        } else {
+            _uiState.value = NoteUiState(showOrAdd = false, topPageText = "نمایش یادداشت")
+        }
+    }
 }
