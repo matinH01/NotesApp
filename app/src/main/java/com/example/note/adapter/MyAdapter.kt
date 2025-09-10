8aace0b46b55fb27603c31fb633ea35c8812f6e1
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-10 09:56:25 +0330

    Update MyAdapter.kt

--- app/src/main/java/com/example/note/adapter/MyAdapter.kt
+++ app/src/main/java/com/example/note/adapter/MyAdapter.kt
@@ -41,7 +41,7 @@ class MyAdapter(
         holder.binding.root.setOnClickListener {
             val intent = Intent(context, MainActivity::class.java)
             intent.putExtra("WhichItem", position)
-            intent.putExtra("Show", true)
+            intent.putExtra("AddOrShow", false)
             context.startActivity(intent)
         }
     }
