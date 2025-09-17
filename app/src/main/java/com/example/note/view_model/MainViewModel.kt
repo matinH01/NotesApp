6e4b02a21226688bb803b397d9adedd7ed005942
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 18:15:13 +0330

    Update MainViewModel.kt

--- app/src/main/java/com/example/note/view_model/MainViewModel.kt
+++ app/src/main/java/com/example/note/view_model/MainViewModel.kt
@@ -1,5 +1,20 @@
 package com.example.note.view_model
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.note.model.NotesData
+import com.example.note.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class MainViewModel @Inject constructor(private val noteRepository: NoteRepository) : ViewModel() {
+    val notes = noteRepository.fetchNotes()
+
+    fun delete(note: NotesData) {
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+    }
 }
