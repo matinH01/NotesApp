919a36bb18b28d3569516e9c42269778875185df
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 18:14:53 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/note/view/MainActivity.kt
+++ app/src/main/java/com/example/note/view/MainActivity.kt
@@ -5,40 +5,49 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.note.R
 import com.example.note.adapter.MyAdapter
-import com.example.note.dao.NotesDao
 import com.example.note.databinding.ActivityMainBinding
 import com.example.note.view_model.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var notesDao: NotesDao
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        notesDao = NotesDatabase.buildDatabase(this).getNotesDao()
-        binding.viewModel = viewModel
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        init()
+    }
+
+    private fun init() {
         binding.lifecycleOwner = this
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MyAdapter(this) { note ->
+            viewModel.delete(note)
+        }
+        binding.recyclerView.adapter = adapter
+        viewModel.notes.observe(this) { list ->
+            adapter.submitList(list)
+        }
         binding.floatingActionButton.setOnClickListener {
             val intent = Intent(this, AddNotesActivity::class.java)
             intent.putExtra("AddOrShow", true)
             startActivity(intent)
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.setData()
-        viewModel.mainUiState.observe(this) { mainUiState ->
-            mainUiState.adapter = MyAdapter(this, selectFromDb())
-            mainUiState.layoutManager = LinearLayoutManager(this)
-        }
-    }
-
-    private fun selectFromDb() = notesDao.getAllNotes()
 }
