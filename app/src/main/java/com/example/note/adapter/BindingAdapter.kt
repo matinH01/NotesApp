0fb6e479c5d7d414dd738b00424cacdda4311bdf
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 18:14:36 +0330

    Update BindingAdapter.kt

--- app/src/main/java/com/example/note/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/note/adapter/BindingAdapter.kt
@@ -1,2 +1,26 @@
 package com.example.note.adapter
 
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat.getString
+import androidx.databinding.BindingAdapter
+import com.example.note.R
+
+@BindingAdapter("setTextRegisterOrEnter")
+fun setText(textView: TextView, appOpenBefore: Boolean) {
+    if (appOpenBefore) {
+        textView.text = getString(textView.context, R.string.enter)
+    } else {
+        textView.text = getString(textView.context, R.string.register)
+    }
+}
+
+@BindingAdapter("setTextNewShow")
+fun setTextAddShow(textView: TextView, newOrShowNote: Boolean) {
+    if (newOrShowNote) {
+        textView.text = getString(textView.context, R.string.newNote)
+    } else {
+        textView.text = getString(textView.context, R.string.showNote)
+    }
+}
