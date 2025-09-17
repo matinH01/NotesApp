a895a7baf570f39556bf7e974adc7d5126304f40
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 13:00:28 +0330

    Update MyAdapter.kt

--- app/src/main/java/com/example/note/adapter/MyAdapter.kt
+++ app/src/main/java/com/example/note/adapter/MyAdapter.kt
@@ -8,8 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.note.databinding.RecyclerLayoutBinding
 import com.example.note.model.NotesData
-import com.example.note.model.NotesDatabase
-import com.example.note.view.MainActivity
+import com.example.note.view.AddNotesActivity
 
 class MyAdapter(
     private val context: Context,
@@ -39,7 +38,7 @@ class MyAdapter(
             notifyItemRangeChanged(position, noteData.size)
         }
         holder.binding.root.setOnClickListener {
-            val intent = Intent(context, MainActivity::class.java)
+            val intent = Intent(context, AddNotesActivity::class.java)
             intent.putExtra("WhichItem", position)
             intent.putExtra("AddOrShow", false)
             context.startActivity(intent)
