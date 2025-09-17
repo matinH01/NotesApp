f8507ef68a05929c3a3f5c259e5fe78bd3e2bcde
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 18:16:00 +0330

    Update NoteRepository.kt

--- app/src/main/java/com/example/note/repository/NoteRepository.kt
+++ app/src/main/java/com/example/note/repository/NoteRepository.kt
@@ -1,4 +1,18 @@
 package com.example.note.repository
 
-class NoteRepository {
+import androidx.lifecycle.LiveData
+import com.example.note.dao.NotesDao
+import com.example.note.model.NotesData
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(val notesDao: NotesDao) {
+     fun fetchNotes(): LiveData<MutableList<NotesData>> = notesDao.getAllNotes()
+
+    suspend fun insertNote(note: NotesData) {
+        notesDao.insertNotes(note)
+    }
+
+    suspend fun deleteNote(note: NotesData) {
+        notesDao.deleteNotes(note)
+    }
 }
