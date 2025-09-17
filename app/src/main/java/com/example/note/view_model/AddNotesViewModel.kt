a00af3c7513374a51b50099e34a1e55033b9f4e6
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 18:13:17 +0330

    Update AddNotesViewModel.kt

--- app/src/main/java/com/example/note/view_model/AddNotesViewModel.kt
+++ app/src/main/java/com/example/note/view_model/AddNotesViewModel.kt
@@ -3,20 +3,44 @@ package com.example.note.view_model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.note.model.NotesData
+import com.example.note.repository.NoteRepository
 import com.example.note.state.AddNoteUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddNotesViewModel @Inject constructor() : ViewModel() {
+class AddNotesViewModel @Inject constructor(
+    private val noteRepository: NoteRepository
+) : ViewModel() {
+    private var whichItem: Int? = null
     private val _uiState = MutableLiveData<AddNoteUiState>()
     val uiState: LiveData<AddNoteUiState> = _uiState
 
     fun setData() {
-        if (_uiState.value?.showOrAdd == true) {
-            _uiState.value = AddNoteUiState(topPageText = "یادداشت جدید")
-        } else {
-            _uiState.value = AddNoteUiState(showOrAdd = false, topPageText = "نمایش یادداشت")
+        viewModelScope.launch {
+            if (_uiState.value?.showOrAdd == true) {
+                _uiState.value = AddNoteUiState()
+                if (_uiState.value!!.title.isNotEmpty() && _uiState.value!!.description.isNotEmpty()) {
+                    noteRepository.insertNote(
+                        NotesData(
+                            title = _uiState.value!!.title,
+                            description = _uiState.value!!.description
+                        )
+                    )
+                }
+            } else {
+                _uiState.value = AddNoteUiState(showOrAdd = false, textEnable = false)
+                whichItem = _uiState.value!!.whichItem
+                if (whichItem != -1) {
+                    _uiState.value?.title =
+                        noteRepository.fetchNotes().value?.get(whichItem!!)?.title.toString()
+                    _uiState.value?.description =
+                        noteRepository.fetchNotes().value?.get(whichItem!!)?.description.toString()
+                }
+            }
         }
     }
 }
