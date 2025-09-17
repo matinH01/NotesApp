3eb95263f1b6b0ff617c4eb092f904e452496750
matinH01 <219396299+matinH01@users.noreply.github.com>
2025-09-17 13:00:15 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/note/view/MainActivity.kt
+++ app/src/main/java/com/example/note/view/MainActivity.kt
@@ -3,35 +3,27 @@ package com.example.note.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.note.R
 import com.example.note.adapter.MyAdapter
+import com.example.note.dao.NotesDao
 import com.example.note.databinding.ActivityMainBinding
-import com.example.note.model.NotesDao
-import com.example.note.model.NotesData
-import com.example.note.model.NotesDatabase
-import com.example.note.viewModel.NotesViewModel
+import com.example.note.view_model.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var notesDao: NotesDao
-    private lateinit var adapter: MyAdapter
-    private val viewModel = NotesViewModel()
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         notesDao = NotesDatabase.buildDatabase(this).getNotesDao()
-        val noteData = selectFromDb()
-
-        setRecyclerViewAdapter(noteData)
-
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-
         binding.floatingActionButton.setOnClickListener {
             val intent = Intent(this, AddNotesActivity::class.java)
             intent.putExtra("AddOrShow", true)
@@ -39,12 +31,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setRecyclerViewAdapter(noteData: MutableList<NotesData>) {
-        adapter = MyAdapter(this, noteData)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = adapter
+    override fun onResume() {
+        super.onResume()
+        viewModel.setData()
+        viewModel.mainUiState.observe(this) { mainUiState ->
+            mainUiState.adapter = MyAdapter(this, selectFromDb())
+            mainUiState.layoutManager = LinearLayoutManager(this)
+        }
     }
 
     private fun selectFromDb() = notesDao.getAllNotes()
-
 }
