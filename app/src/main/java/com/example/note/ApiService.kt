c7539e2d7c02ce148e2285d83683d1b7dc9fb066
matinH01 <matinhamdolahi117@gmail.com>
2025-09-26 15:32:48 +0330

    Delete ApiService.kt

--- app/src/main/java/com/example/note/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.note
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("Api/Market/Gold_Currency.php?key=Freeb4kFJyIjKGneMaRtkAE457lV9iON")
-    fun getData(): Call<String>
-}
