eadd6586f1f5db0ce2708ec0e90c78fa0594fdd7
matinH01 <matinhamdolahi117@gmail.com>
2025-09-26 15:39:33 +0330

    Update LoginViewModel.kt

--- app/src/main/java/com/example/note/view_model/LoginViewModel.kt
+++ app/src/main/java/com/example/note/view_model/LoginViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.note.view_model
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.note.state.LoginUiState
+
+class LoginViewModel : ViewModel() {
+    private val _loginUiState = MutableLiveData<LoginUiState>()
+    val loginUiState: LiveData<LoginUiState> = _loginUiState
+    private var username: String = ""
+    private var password: String = ""
+
+    // for updating fields
+    fun onUsernameChanged(user: String) {
+        username = user
+    }
+
+    fun onPasswordChanged(pass: String) {
+        password = pass
+    }
+
+    fun checkData() {
+        _loginUiState.value = LoginUiState(username, password)
+    }
+
 }
